9f80e25ece9a2078274f4e10971fcfd5c768e3b8
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2015-04-02 15:22:22 +0900

    fix #2: implement tf-idf variants described in http://nlp.stanford.edu/IR-book/html/htmledition/variant-tf-idf-functions-1.html

--- src/main/scala/org/nlp4l/stats/TFIDF.scala
+++ src/main/scala/org/nlp4l/stats/TFIDF.scala
@@ -16,7 +16,7 @@
 
 package org.nlp4l.stats
 
-import org.nlp4l.core.IReader
+import org.nlp4l.core.{RawReader, IReader}
 
 import scala.collection.immutable.TreeMap
 
@@ -34,15 +34,8 @@ object TFIDF {
    * @return the Vector of words and the feature vector
    */
   def tfVector(reader: IReader, field: String, docId: Int, words: Set[String] = Set.empty): (Vector[String], Vector[Long]) = {
-    if (words.isEmpty) {
-      val tMap = TreeMap(WordCounts.count(reader, field, words, Set(docId)).toArray: _*)
-      (tMap.keys.toVector, tMap.values.toVector)
-    } else {
-      val countMap = WordCounts.count(reader, field, words, Set(docId))
-      val features = words.toSeq.sorted
-      val vector = features.map(w => countMap.getOrElse(w, 0L)).toVector
-      (features.toVector, vector)
-    }
+    val (features, vector) =  tfIdfVector(reader, field, docId, words, tfMode="n", idfMode="n")
+    (features, vector.map(_.toLong))
   }
 
   /**
@@ -51,17 +44,20 @@ object TFIDF {
    * @param field the field name for counting words
    * @param docIds  the list of Lucene document id
    * @param words the set of words(terms) considered as feature. All words(terms) will be taken as features if empty set is given.
-   * @return the Vector of words and the feature vectors
+   * @return the pair of words and the feature vectors
    */
   def tfVectors(reader: IReader, field: String, docIds: List[Int], words: Set[String] = Set.empty): (Vector[String], List[Vector[Long]]) = {
-    val countMaps = docIds.map(id => WordCounts.count(reader, field, words, Set(id)))
-    val mergedWords = if (words.isEmpty) countMaps.flatMap(_.keys).toSet.toSeq.sorted else words.toSeq.sorted
-    val vectors = countMaps.map(map => mergedWords.map(w => map.getOrElse(w, 0L)).toVector)
-    (mergedWords.toVector, vectors)
+    val (features, vectors) = tfIdfVectors(reader, field, docIds, words, tfMode="n", idfMode="n")
+    (features, vectors.map(_.map(_.toLong)))
   }
 
   /**
-   * Generate basic tf-idf based feature vector from a document. Weight for each term is given by
+   * Generate tf-idf based feature vector from a document.
+   *
+   * tf and idf calculations can be varied according to "tfMode" and "idfMode" parameters.
+   * See http://nlp.stanford.edu/IR-book/html/htmledition/variant-tf-idf-functions-1.html for theoretical backgrounds.
+   *
+   * In default, when tfMode and idfMode are not given, weight for each term is given by this basic tf-idf formula
    *
    *  (tf) * log(N / df)
    *
@@ -71,30 +67,114 @@ object TFIDF {
    * @param field the field name for counting words
    * @param docId the Lucene document id
    * @param words the set of words(terms) considered as feature. All words(terms) will be taken as features if empty set is given.
+   * @param tfMode tf calculation mode. Expected values are "n" (normal), "l" (logarithm), "m" (maximum normalization), "b" (boolean), "L" (Log ave), "w" (sublinear weighted). The default value is "n"
+   * @param a the smoothing term for tfMode "m". The default value is 0.4.
+   * @param idfMode idf calculation mode. Expected values are "n" (no), "t" (idf), "p" (prob idf). The default value is "t"
    * @return the Vector of words and the feature vector
    */
-  def tfIdfVector(reader: IReader, field: String, docId: Int, words: Set[String] = Set.empty): (Vector[String], Vector[Double]) = {
+  def tfIdfVector(reader: IReader, field: String, docId: Int, words: Set[String] = Set.empty, tfMode: String = "n", a: Double = 0.4, idfMode: String = "t"): (Vector[String], Vector[Double]) = {
+    val numDocs = Some(reader.numDocs)
+    val maxTF = if (tfMode == "m") Some(reader.topTermsByTotalTermFreq(field, 1)(0)._3) else None
+    val countMap = WordCounts.count(reader, field, words, Set(docId))
+    val aveTF = if (tfMode == "L") Some(Stats.average(countMap.values)) else None
     if (words.isEmpty) {
-      val countMap = WordCounts.count(reader, field, words, Set(docId))
       val dfMap = WordCounts.countDF(reader, field, countMap.keys.toSet)
-      val tfIdfMap = countMap.map(e => (e._1, e._2 * math.log(reader.numDocs / dfMap(e._1).toDouble)))
+      val tfIdfMap = countMap.map(e => (e._1, tf(e._2, tfMode, maxTF, a, aveTF) * idf(dfMap(e._1), idfMode, numDocs)))
       val tMap = TreeMap(tfIdfMap.toArray: _*)
       (tMap.keys.toVector, tMap.values.toVector)
     } else {
-      val countMap = WordCounts.count(reader, field, words, Set(docId))
       val dfMap = WordCounts.countDF(reader, field, words)
       val features = words.toSeq.sorted
-      val vector = features.map(w => if (countMap.contains(w)) countMap(w) * math.log(reader.numDocs / dfMap(w).toDouble) else 0.0).toVector
+      val vector = features.map(w => if (countMap.contains(w)) tf(countMap(w), tfMode, maxTF, a, aveTF) * idf(dfMap(w), idfMode, numDocs) else 0.0).toVector
       (features.toVector, vector)
     }
   }
 
-  def tfIdfVectors(reader: IReader, field: String, docIds: List[Int], words: Set[String] = Set.empty): (Vector[String], List[Vector[Double]]) = {
+  /**
+   * Generate tf-idf based feature vector from a document.
+   *
+   * See also documentation for tfIdfVector().
+   *
+   * @param reader the IReader instance
+   * @param field the field name for counting words
+   * @param docIds the list of Lucene document id
+   * @param words the set of words(terms) considered as feature. All words(terms) will be taken as features if empty set is given.
+   * @param tfMode tf calculation mode. The default value is "n"
+   * @param a the smoothing term for tfMode "m". The default value is 0.4.
+   * @param idfMode idf calculation mode. The default value is "t"
+   * @return the pair of words and the feature vectors
+   */
+  def tfIdfVectors(reader: IReader, field: String, docIds: List[Int], words: Set[String] = Set.empty, tfMode: String = "n", a: Double = 0.4, idfMode: String = "t"): (Vector[String], List[Vector[Double]]) = {
+    val numDocs = Some(reader.numDocs)
+    val maxTF = if (tfMode == "m") Some(reader.topTermsByTotalTermFreq(field, 1)(0)._3) else None
     val countMaps = docIds.map(id => WordCounts.count(reader, field, words, Set(id)))
     val dfMap = if (words.isEmpty) WordCounts.countDF(reader, field, countMaps.flatMap(_.keys).toSet) else WordCounts.countDF(reader, field, words)
     val mergedWords = if (words.isEmpty) countMaps.flatMap(_.keys).toSet.toSeq.sorted else words.toSeq.sorted
-    val vectors = countMaps.map(map => mergedWords.map(w => if (map.contains(w)) map(w) * math.log(reader.numDocs / dfMap(w).toDouble) else 0.0).toVector)
+    val vectors = countMaps.map(map => {
+      val aveTF = if (tfMode == "L") Some(Stats.average(map.values)) else None
+      mergedWords.map(w => if (map.contains(w)) tf(map(w), tfMode, maxTF, a, aveTF) * idf(dfMap(w), idfMode, numDocs) else 0.0).toVector
+    })
     (mergedWords.toVector, vectors)
   }
 
+  /**
+   * Calculate TF variants.
+   *
+   * @param v the natural TF value for the term
+   * @param mode the notation for each variant
+   * @param maxTF the max term frequency in the whole documents. Required when "m" is given to mode.
+   * @param a the smoothing term for tfMode "m".
+   * @param aveTF the average of TF in the document. Required when "L" is given to mode.
+   * @return the TF value
+   */
+  private def tf(v: Long, mode: String, maxTF: Option[Long] = None, a: Double = 0.4, aveTF: Option[Double] = None): Double = mode match {
+    // normal
+    case "n" => v.toDouble
+    // logarithm
+    case "l" => 1 + math.log(v)
+    // maximum tf normalization
+    case "m" => {
+      if (a < 0.0 || a > 1.0) throw new IllegalArgumentException("a parameter must be in between 0.0 to 1.0")
+      maxTF match {
+        case Some(value) => a + (a * v) / value.toDouble
+        case _ => throw new IllegalArgumentException("maxTF parameter is required.")
+      }
+    }
+    // boolean
+    case "b" => if (v > 0) 1.0 else 0.0
+    // Log ave
+    case "L" => aveTF match {
+      case Some(value) => (1 + math.log(v)) / (1 + math.log(value))
+      case _ => throw new IllegalArgumentException("aveTF parameter is required.")
+    }
+    // sublinear weighted tf
+    case "w" => if (v > 0) 1 + math.log(v) else 0.0
+    // unknown
+    case _ => throw new IllegalArgumentException("Unknown notation: " + mode)
+  }
+
+  /**
+   * Calculate IDF variants.
+   *
+   * @param v the document frequency for the term
+   * @param mode the notation for each variant
+   * @param numDocs the total number of documents. Required when "t" or "p" is given to notation.
+   * @return the IDF value
+   */
+  private def idf(v: Long, mode: String, numDocs: Option[Int] = None): Double = mode match {
+    // no
+    case "n" => 1
+    // idf
+    case "t" => numDocs match {
+      case Some(value) => math.log(value / v.toDouble)
+      case _ => throw new IllegalThreadStateException("numDocs parameter is required.")
+    }
+    // prob idf
+    case "p" => numDocs match {
+      case Some(value) => math.max(0, math.log((value - v) / v.toDouble))
+      case _ => throw new IllegalThreadStateException("numDocs parameter is required.")
+    }
+    // unknown
+    case _ => throw new IllegalArgumentException("Unknown notation: " + mode)
+  }
 }
